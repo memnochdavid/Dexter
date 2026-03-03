858c4f4e966b7a52e13864a7771dd156d56f67af
David Duque <davduquediaz@gmail.com>
2026-03-03 19:29:48 +0100

    carga async de listas

--- app/src/main/java/com/david/pokedex_api/api/client/RetrofitClient.kt
+++ app/src/main/java/com/david/pokedex_api/api/client/RetrofitClient.kt
@@ -1,15 +1,31 @@
 package com.david.pokedex_api.api.client
 
 import com.david.pokedex_api.api.service.PokeApiService
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
+    // Aumentamos el número de conexiones simultáneas permitidas
+    private val dispatcher = Dispatcher().apply {
+        maxRequests = 100
+        maxRequestsPerHost = 20
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .dispatcher(dispatcher)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
     val instance: PokeApiService by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         retrofit.create(PokeApiService::class.java)
